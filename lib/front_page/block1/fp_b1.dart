765abc64e991a871406cca005caaf90d3c1f87d7
temk <artkhors@gmail.com>
2016-03-17 16:42:33 +0300

    commit message

--- lib/front_page/block1/fp_b1.dart
+++ lib/front_page/block1/fp_b1.dart
@@ -11,37 +11,6 @@ import 'dart:html';
 )
 class Block1 {
 
-//  Map b1Style;
-//  Map b2Style;
-//  final Map noShadowStyle;
-//
-//  Block1()
-//      :noShadowStyle={'box-shadow' : 'none'},
-//        b1Style = {},
-//        b2Style = {};
-
-//  b1Mousedown(String color) {
-//    b1Style = {
-//      'background-color' :
-//    };
-//    b1Style.addAll(noShadowStyle);
-//  }
-//
-//  b1Mouseup() {
-//    b1Style.clear();
-//  }
-//
-//  b2Mousedown() {
-//    b2Style = {
-//      'background-color' :
-//    };
-//    b2Style.addAll(noShadowStyle);
-//  }
-//
-//  b2Mouseup() {
-//    b2Style.clear();
-//  }
-
   bMousedown(Event event, String color) {
     (event.currentTarget as Element).attributes.addAll({
       'style' : 'background-color: $color; box-shadow: none'
--- lib/front_page/front_page.dart
+++ lib/front_page/front_page.dart
@@ -1,7 +1,6 @@
 library semplex.client.front_page;
 
 import 'package:angular2/angular2.dart';
-//import 'dart:html';
 
 import 'block1/fp_b1.dart';
 
@@ -12,26 +11,5 @@ import 'block1/fp_b1.dart';
     styleUrls: const['front_page.css'],
     directives: const [CORE_DIRECTIVES, FORM_DIRECTIVES, Block1]
 )
-class FrontPage
-//    implements OnInit
-{
-
-//  ElementRef _hostRef;
-//  Element _host;
-//
-//  FrontPage(this._hostRef) {
-//    _host=_hostRef.nativeElement as Element;
-//  }
-
-//  ngOnInit(){
-//    Element elemFB_B1 = _host.querySelector('fp-b1');
-//    elemFB_B1.attributes['style'] = '"background-image: url(1.jpg);"';
-//  }
-
-
-
-//  Map get dynamicStyles => {
-//    'background-image' : 'url(1.jpg)'
-//  };
-
+class FrontPage {
 }
